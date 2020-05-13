fe7ce4438956b1c9669da6f212f86972786f8ee1
DjCtavia <flan-etienne520@live.com>
2020-05-13 09:18:40 +0200

    Create image.c

---
+++ Addons/Scripts/3_Game/objects/children/image.c
@@ -0,0 +1,64 @@
+class DiscordObject_Image : DiscordObject
+{
+	/*
+		https://discord.com/developers/docs/resources/channel#embed-object-embed-image-structure
+
+		Fields:
+			url - String
+			proxy_url - string
+			height - int
+			width - int
+	*/
+	private int m_width;
+	private int m_height;
+
+	void DiscordFooter_Footer(int width = -1, int height = -1)
+	{
+		m_width = width;
+		m_height = height;
+	}
+	
+	override string DefaultJSONConvert()
+	{
+		return ConvertToJsonCloseByObject();
+	}
+	
+	override protected string ConvertSpecialFields()
+	{
+		string addedFieldsToJson = "";
+		
+		addedFieldsToJson += GetWidthField();
+		addedFieldsToJson += GetHeightField();
+		return addedFieldsToJson;
+	}
+	
+	private string GetWidthField()
+	{
+		if (m_width > -1)
+		{
+			return helperJSON.GetJSONLineOthers("width", m_width.ToString());
+		}
+		return "";
+	}
+	
+	private string GetHeightField()
+	{
+		if (m_height > -1)
+		{
+			return helperJSON.GetJSONLineOthers("height", m_height.ToString());
+		}
+		return "";
+	}
+
+	override protected bool CanSetKey(string key)
+	{
+		switch (key)
+		{
+			case "url":
+				return true;
+			case "proxy_url":
+				return true;
+		}
+		return false;
+	}
+};
