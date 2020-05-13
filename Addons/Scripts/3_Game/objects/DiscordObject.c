061e8ab7a26d46eaf871af837ea0ec8d57464cc1
DjCtavia <flan-etienne520@live.com>
2020-05-13 09:17:06 +0200

    Filled DiscordObject class with more methods

--- Addons/Scripts/3_Game/objects/DiscordObject.c
+++ Addons/Scripts/3_Game/objects/DiscordObject.c
@@ -7,18 +7,75 @@ class DiscordObject
 		m_fields = new map<string, string>;
 	}
 	
-	string ConvertToJson()
+	// Override this function for overriding the default method
+	string DefaultJSONConvert()
+	{
+		return ConvertToJson();
+	}
+
+	protected string ConvertToJson()
+	{
+		string jsonData = "";
+		
+		foreach (string key : m_fields.GetKeyArray())
+		{
+			jsonData += FormatField(key);
+		}
+		jsonData += ConvertSpecialFields();
+		return jsonData;
+	}
+
+	protected string ConvertToJsonCloseByObject()
 	{
 		string jsonData = "{";
-		foreach (string key : m_object.GetKeyArray())
+		
+		foreach (string key : m_fields.GetKeyArray())
 		{
-			jsonData += FormatKey(key);
+			jsonData += FormatField(key);
 		}
-		jsonData += "}";
+		jsonData += ConvertSpecialFields() + "}";
+		return jsonData;
+	}
+
+	protected string ConvertToJsonCloseByArray()
+	{
+		string jsonData = "[";
+		
+		foreach (string key : m_fields.GetKeyArray())
+		{
+			jsonData += FormatField(key);
+		}
+		jsonData += ConvertSpecialFields() + "]";
+		return jsonData;
+	}
+
+	// It need to be override in children
+	protected string ConvertSpecialFields() {return "";}
+	
+	protected string FormatField(string key)
+	{
+		return helperJSON.GetJSONLineString(key, m_fields.Get(key));
 	}
 	
-	protected string FormatKey(string key)
+	// Can limit what key name can be include in children class if modified
+	protected bool CanSetKey(string key)
 	{
-		return "\"" + key + "\":" + "\"" + m_object.Get(key) + "\"";
+		return true;
+	}
+	
+	protected void SetField(string field, string value)
+	{
+		if (CanSetKey())
+		{
+			m_fields.Set(field, value);
+		}
+	}
+	
+	protected void RemoveField(string field)
+	{
+		if (m_fields.Contains(field))
+		{
+			m_fields.Remove(field);
+		}
 	}
 };
